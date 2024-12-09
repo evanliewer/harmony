a7f70efd270e65a8ddb822d991d46f6084c98469
Evan Liewer <evanliewer@gmail.com>
2024-12-08 20:55:58 -0800

    removed host_id script

--- db/migrate/20241011052304_rename_host_id_to_tag_id_in_items_applied_tags.rb
+++ db/migrate/20241011052304_rename_host_id_to_tag_id_in_items_applied_tags.rb
@@ -1,7 +1,7 @@
 class RenameHostIdToTagIdInItemsAppliedTags < ActiveRecord::Migration[7.1]
   def change
-    # Rename the column
-    rename_column :items_applied_tags, :host_id, :tag_id
+    # Rename the column commented out to upload to heroku
+   # rename_column :items_applied_tags, :host_id, :tag_id
 
     # Rename the index if needed
     #rename_index :items_applied_tags, :index_items_applied_tags_on_host_id, :index_items_applied_tags_on_tag_id
