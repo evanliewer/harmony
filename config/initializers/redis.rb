7ae97231b4b88e2f780020f6a560d88b7388c8be
Evan Liewer <evanliewer@gmail.com>
2024-12-09 16:44:19 -0800

    switch to true

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,7 @@
-if heroku?
- # $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}) # standard:disable Style/GlobalVars
-  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+if true
+  require 'redis'
+  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}) # standard:disable Style/GlobalVars
+#  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+
 end
